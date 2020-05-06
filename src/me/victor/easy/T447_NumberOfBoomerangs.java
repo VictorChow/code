bcaecc41fc9b845ed4fa4ebc84d2c1368f43d044
Victor Chow <285050252@qq.com>
2020-05-06 17:50:54 +0800

    update

---
+++ src/me/victor/easy/T447_NumberOfBoomerangs.java
@@ -0,0 +1,29 @@
+package me.victor.easy;
+
+import java.util.HashMap;
+
+/**
+ * Created by victor on 2020/5/6. (ง •̀_•́)ง
+ */
+
+public class T447_NumberOfBoomerangs {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static int original(int[][] points) {
+        var hashMap = new HashMap<Integer, Integer>();
+        int count = 0;
+        for (int i = 0; i < points.length; i++) {
+            hashMap.clear();
+            for (int j = 0; j < points.length; j++) {
+                if (i == j) continue;
+                int distance = (points[i][0] - points[j][0]) * (points[i][0] - points[j][0]) + (points[i][1] - points[j][1]) * (points[i][1] - points[j][1]);
+                count += hashMap.getOrDefault(distance, 0) * 2;
+                hashMap.put(distance, hashMap.getOrDefault(distance, 0) + 1);
+            }
+        }
+        return count;
+    }
+}
