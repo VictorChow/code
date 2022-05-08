5fe9f81557ceb810b51a2c999fd2409d059b34d1
Victor Chow <285050252@qq.com>
2022-05-08 19:19:52 +0800

    update

--- src/me/victor/code/T13_Roman2Integer.java
+++ src/me/victor/code/T13_Roman2Integer.java
@@ -125,23 +125,15 @@ public class T13_Roman2Integer {
     }
 
     private static int getValue(char ch) {
-        switch (ch) {
-            case 'I':
-                return 1;
-            case 'V':
-                return 5;
-            case 'X':
-                return 10;
-            case 'L':
-                return 50;
-            case 'C':
-                return 100;
-            case 'D':
-                return 500;
-            case 'M':
-                return 1000;
-            default:
-                return 0;
-        }
+        return switch (ch) {
+            case 'I' -> 1;
+            case 'V' -> 5;
+            case 'X' -> 10;
+            case 'L' -> 50;
+            case 'C' -> 100;
+            case 'D' -> 500;
+            case 'M' -> 1000;
+            default -> 0;
+        };
     }
 }
