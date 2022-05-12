46297b9c19bc6393facefba415f32d0f6c51c1de
Victor Chow <285050252@qq.com>
2022-05-12 23:26:11 +0800

    update

--- src/me/victor/code/T30_SubstringWithConcatenationOfAllWords.java
+++ src/me/victor/code/T30_SubstringWithConcatenationOfAllWords.java
@@ -13,40 +13,45 @@ public class T30_SubstringWithConcatenationOfAllWords {
 
     public static void main(String[] args) {
         var t = new T30_SubstringWithConcatenationOfAllWords();
-        //        System.out.println(t.findSubstring("barfoothefoobarman", Util.strArray("foo", "bar")));
-        //        System.out.println(t.findSubstring("wordgoodgoodgoodbestword", Util.strArray("word", "good", "best", "word")));
-        //        System.out.println(t.findSubstring("barfoofoobarthefoobarman", Util.strArray("bar", "foo", "the")));
+        System.out.println(t.findSubstring("barfoothefoobarman", Util.strArray("foo", "bar")));
+        System.out.println(t.findSubstring("wordgoodgoodgoodbestword", Util.strArray("word", "good", "best", "word")));
+        System.out.println(t.findSubstring("barfoofoobarthefoobarman", Util.strArray("bar", "foo", "the")));
         System.out.println(t.findSubstring("wordgoodgoodgoodbestword", Util.strArray("word", "good", "best", "good")));
 
     }
 
     public List<Integer> findSubstring(String s, String[] words) {
         var len = words[0].length();
+        var wordList = List.of(words);
         if (s.length() < len * words.length) return Collections.emptyList();
         var indices = new ArrayList<Integer>();
         var keys = new ArrayList<>(List.of(words));
         for (int l = 0; l < len; l++) {
-            renewKeys(keys, words);
+            renewKeys(keys, wordList);
             var i = l;
             while (i + len <= s.length()) {
                 var str = s.substring(i, i += len);
-                if (keys.remove(str)) {
-                    if (keys.isEmpty()) {
-                        indices.add(i - len * words.length);
-                        renewKeys(keys, words);
-                        i -= len * (words.length - 1);
+                if (wordList.contains(str))
+                    if (keys.remove(str)) {
+                        if (keys.isEmpty()) {
+                            indices.add(i - len * words.length);
+                            renewKeys(keys, wordList);
+                            i -= len * (words.length - 1);
+                        }
+                    } else {
+                        i -= (words.length - keys.size()) * len;
+                        renewKeys(keys, wordList);
                     }
-                } else {
-                    i -= (words.length - keys.size()) * len;
-                    renewKeys(keys, words);
+                else {
+                    renewKeys(keys, wordList);
                 }
             }
         }
         return indices;
     }
 
-    private void renewKeys(List<String> list, String[] words) {
+    private void renewKeys(List<String> list, List<String> wordList) {
         list.clear();
-        list.addAll(List.of(words));
+        list.addAll(wordList);
     }
 }
---
+++ src/me/victor/code/T31_NextPermutation.java
@@ -0,0 +1,81 @@
+package me.victor.code;
+
+import java.util.Arrays;
+
+import me.victor.code.util.Util;
+
+/**
+ * https://leetcode.cn/problems/next-permutation/
+ */
+class T31_NextPermutation {
+
+    public static void main(String[] args) {
+        var t = new T31_NextPermutation();
+
+        t.nextPermutation(Util.intArray(1));
+        t.nextPermutation(Util.intArray(2, 1));
+        t.nextPermutation(Util.intArray(1, 2, 3));
+        t.nextPermutation(Util.intArray(3, 2, 1));
+        t.nextPermutation(Util.intArray(1, 1, 5));
+        t.nextPermutation(Util.intArray(1, 1, 1, 2, 2, 3, 4, 1, 5, 2, 2, 2, 2, 2));
+        t.nextPermutation(Util.intArray(2, 6, 5, 3, 3, 1));
+        t.nextPermutation(Util.intArray(4, 2, 0, 2, 3, 2, 0));
+    }
+
+    public void nextPermutation(int[] nums) {
+        System.out.println(Arrays.toString(nums));
+        if (nums.length <= 1) return;
+        for (int i = nums.length - 1; i >= 1; i--) {
+            if (nums[i - 1] < nums[i]) {
+                var smallerIdx = findMinSmallerIdx(nums, nums[i - 1], i);
+                swap(nums, smallerIdx, i - 1);
+                Arrays.sort(nums, i, nums.length);
+                System.out.println(Arrays.toString(nums));
+                return;
+            }
+        }
+        for (int i = 0; i < nums.length / 2; i++) {
+            var j = nums.length - 1 - i;
+            swap(nums, i, j);
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+
+    private int findMinSmallerIdx(int[] nums, int cur, int l) {
+        int val = Integer.MAX_VALUE, idx = l;
+        for (int i = l; i < nums.length; i++) {
+            if (nums[i] > cur && nums[i] < val) {
+                idx = i;
+            }
+        }
+        return idx;
+    }
+
+    public void nextPermutation2(int[] nums) {
+        if (nums.length <= 1) return;
+        System.out.println(Arrays.toString(nums));
+        int idx = nums.length - 2;
+        var preIdx = idx + 1;
+        while (idx >= 0) {
+            if (nums[preIdx] > nums[idx]) {
+                swap(nums, preIdx, idx);
+                System.out.println(Arrays.toString(nums));
+                return;
+            } else if (nums[idx] == nums[preIdx]) {
+                preIdx--;
+            }
+            idx--;
+        }
+        for (int i = 0; i < nums.length / 2; i++) {
+            var j = nums.length - 1 - i;
+            swap(nums, i, j);
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        nums[i] ^= nums[j];
+        nums[j] ^= nums[i];
+        nums[i] ^= nums[j];
+    }
+}
