237479c17f909aabda341a0c712052bb8c587273
Victor Chow <285050252@qq.com>
2022-05-30 22:07:34 +0800

    update

--- src/me/victor/code/T79_WordSearch.java
+++ src/me/victor/code/T79_WordSearch.java
@@ -12,28 +12,42 @@ class T79_WordSearch {
         //                {'S', 'F', 'C', 'S'},
         //                {'A', 'D', 'E', 'E'}
         //        }, "ABCCED"));
+        //
+        //        System.out.println(t.exist(new char[][]{
+        //                {'A', 'B', 'C', 'E'},
+        //                {'S', 'F', 'C', 'S'},
+        //                {'A', 'D', 'E', 'E'}
+        //        }, "SEE"));
+
+        //        System.out.println(t.exist(new char[][]{
+        //                {'A', 'B', 'C', 'E'},
+        //                {'S', 'F', 'C', 'S'},
+        //                {'A', 'D', 'E', 'E'}
+        //        }, "ABCB"));
 
         System.out.println(t.exist(new char[][]{
-                {'A', 'B', 'C', 'E'},
-                {'S', 'F', 'C', 'S'},
-                {'A', 'D', 'E', 'E'}
-        }, "SEE"));
+                {'A'}
+        }, "A"));
     }
 
     public boolean exist(char[][] board, String word) {
-        return exist(board, 0, 0, new boolean[board.length][board[0].length], word.toCharArray(), 0);
+        var ch = word.charAt(0);
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == ch) {
+                    if (exist(board, i, j, new boolean[board.length][board[0].length], word.toCharArray(), 0)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     public boolean exist(char[][] board, int r, int c, boolean[][] flags, char[] words, int idx) {
         if (r >= board.length || c >= board[0].length || r < 0 || c < 0) return false;
-//        if (flags[r][c]) {
-//            return exist(board, r + 1, c, flags, words, idx) ||
-//                    exist(board, r - 1, c, flags, words, idx) ||
-//                    exist(board, r, c + 1, flags, words, idx) ||
-//                    exist(board, r, c - 1, flags, words, idx);
-//        }
-        if (idx == words.length) return true;
-        if (board[r][c] != words[idx]) return false;
+        if (idx == words.length - 1) return true;
+        if (flags[r][c] || board[r][c] != words[idx]) return false;
         flags[r][c] = true;
         var b = exist(board, r + 1, c, flags, words, idx + 1) ||
                 exist(board, r - 1, c, flags, words, idx + 1) ||
